8e7c508ef3358840639e4acf7625464f6ca45ae7
atif044 <74341827+atif044@users.noreply.github.com>
2024-06-03 03:55:53 +0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/v1'||'/api/v1'; 
+const API_URL = 'http://localhost:5000/api/v1'||'/api/v1'||'https://harmony-hub-backend.vercel.app/api/v1';
 
 const api = axios.create({
   baseURL: API_URL,
